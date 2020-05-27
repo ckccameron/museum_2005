9ee15e98ee001c01dc790684e4806af1213309f8
Cam C <ckc.cameron@gmail.com>
2020-05-27 09:22:02 -0600

    Passes setup and attributes test; failing add_interests method

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -12,9 +12,9 @@ class PatronTest < Minitest::Test
   def test_it_has_readable_patron_attributes
     patron_1 = Patron.new("Bob", 20)
 
-    assert_equal "Bob", patron.name
-    assert_equal 20, patron.spending_money
-    assert_equal [], patron.interests
+    assert_equal "Bob", patron_1.name
+    assert_equal 20, patron_1.spending_money
+    assert_equal [], patron_1 .interests
   end
 
   def test_it_can_add_interests
