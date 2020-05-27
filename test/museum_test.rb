8aeffd9c73b8b762a3e13386c182b756278a1908
Cam C <ckc.cameron@gmail.com>
2020-05-27 10:30:03 -0600

    Reorganizes suite to have test setup method

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -5,56 +5,50 @@ require './lib/patron'
 require './lib/museum'
 
 class MuseumTest < Minitest::Test
-  def test_it_exists
-    dmns = Museum.new("Denver Museum of Nature and Science")
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
 
-    assert_instance_of Museum, dmns
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
+
+
+  end
+  def test_it_exists
+
+    assert_instance_of Museum, @dmns
   end
 
   def test_it_has_readable_museum_attributes
-    dmns = Museum.new("Denver Museum of Nature and Science")
 
-    assert_equal "Denver Museum of Nature and Science", dmns.name
-    assert_equal [], dmns.exhibits
-    assert_equal [], dmns.patrons
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+    assert_equal [], @dmns.exhibits
+    assert_equal [], @dmns.patrons
   end
 
   def test_it_can_add_exhibits
-    dmns = Museum.new("Denver Museum of Nature and Science")
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
 
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
-
-    dmns.add_exhibit(gems_and_minerals)
-    dmns.add_exhibit(dead_sea_scrolls)
-    dmns.add_exhibit(imax)
-
-    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
   end
 
   def test_it_can_recommend_exhibits_based_on_patron_interests
     skip
-    dmns = Museum.new("Denver Museum of Nature and Science")
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
 
-    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
-    imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
 
-    dmns.add_exhibit(gems_and_minerals)
-    dmns.add_exhibit(dead_sea_scrolls)
-    dmns.add_exhibit(imax)
+    @patron_2.add_interest("IMAX")
 
-    patron_1 = Patron.new("Bob", 20)
-
-    patron_1.add_interest("Gems and Minerals")
-    patron_1.add_interest("Dead Sea Scrolls")
-
-    patron_2 = Patron.new("Sally", 20)
-
-    patron_2.add_interest("IMAX")
-
-    assert_equal [gems_and_minerals, dead_sea_scrolls], dmns.recommend_exhibits(patron_1)
-    assert_equal [imax], dmns.recommend_exhibits(patron_2)
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@patron_1)
+    assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
   end
 end
